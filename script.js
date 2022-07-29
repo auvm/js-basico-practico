ed7ee0b83d224601d616c40e98dbf3d83250e7ea
auvm <angelurielvelascom@gmail.com>
2022-07-28 19:38:07 -0500

    clases de eventos y ejercicio de eventos

--- script.js
+++ script.js
@@ -1,62 +1,27 @@
 const h1 = document.querySelector('h1');
-const p = document.querySelector('p');
-const parrafito = document.getElementsByClassName('parrafito');
-const pid = document.getElementById('pid');
-const input = document.querySelector('input');
-
-console.log(h1);
-
-console.log({
-    h1,
-    p, 
-    parrafito,
-    pid,
-    input
-});
-
-input.value = "entrada";
-
-//inner lo transforma directamente a html
-h1.innerHTML = "Otro texto <br/>"; //no es bueno para la seguridad
-
-//intercambia el texto sin convertirlo a html
-h1.innerText = 'Otro <br/> texto';
-//previene ataques, así no se
-//inyecta código 
+const form = document.querySelector('#form');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btnCalculo = document.querySelector("#btnCalcular");
+const result = document.getElementById('result');
 
 
-//para leer y asignar atributos
-// get y set Attribute
-console.log(h1.getAttribute('pantalla'));//lee el atributo pantalla
-h1.setAttribute('class', 'rojo'); //asigna rojo al atributo class
+// agregar listener
+// btnCalculo.addEventListener('nom_del_evento', 'metodo_de_js_a_ejecutar');
+btnCalculo.addEventListener('click', btnClicked);
 
+//al meter los inputs en un form ahora se debe escuchar
+//al formulario, a su evento submit
+// form.addEventListener('submit', formSubmit);
 
-//manipulación de las clases
-h1.classList.add("header-one"); //agrega
-h1.classList.remove("rojo"); //elimina
+// function formSubmit(event){
+//     // console.log({event});
+//     event.preventDefault();//evita el recargar default de la página
+//     const suma = Number(input1.value) + Number(input2.value);
+//     result.innerText = "Resultado: " + suma;
+//}
 
-
-//h1.classList.toggle();
-// h1.classList.contains();
-
-
-
-//asigna un valor al input
-input.value = 456;
-
-//creo una variable donde almaceno el elemento html img
-const img = document.createElement('img');
-//le asigno atributos que corresponden a las imágenes
-img.setAttribute('src', 'https://adventjs.dev/platzi.jpg');
-img.setAttribute('width', '100px');
-
-//imprimo el elemento en la consola y ya se puede ver
-//la etiqueta con los atributos
-console.log(img);
-
-pid.innerHTML = "";
-
-//agrego el elemento almacenado en la variable
-//a un elemento que si existe en el html
-pid.append(img);
-
+function btnClicked(){
+    const suma = Number(input1.value) + Number(input2.value);
+    result.innerText = "Resultado: " + suma;
+}
